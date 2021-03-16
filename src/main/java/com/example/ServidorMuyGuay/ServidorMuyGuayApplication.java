27e1a572a50d9012276184d89f4072f0798d86c9
Bojaco <joidoyaga13@gmail.com>
2021-03-16 18:40:38 +0100

    primera implementacion

---
+++ src/main/java/com/example/ServidorMuyGuay/ServidorMuyGuayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServidorMuyGuay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorMuyGuayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorMuyGuayApplication.class, args);
+	}
+
+}
